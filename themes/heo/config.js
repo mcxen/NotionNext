3ab0e88f3f3e10b2c9bb182f394ec6275786e71a
MCXEN <83444002+mcxen@users.noreply.github.com>
2024-06-18 14:14:10 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,10 +4,7 @@ const CONFIG = {
   HEO_SITE_CREATE_TIME: '2024-06-18', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-  HEO_NOTICE_BAR: [
-    // { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    // { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
-  ],
+  HEO_NOTICE_BAR: [],
 
   // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: false,
@@ -24,13 +21,13 @@ const CONFIG = {
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: 'Java笔试', url: '/tag/Java笔试' },
-  HEO_HERO_CATEGORY_2: { title: '数字移民, url: '/tag/数字移民' },
+  HEO_HERO_CATEGORY_2: { title: '数字移民', url: '/tag/数字移民' },
   HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
+ HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
